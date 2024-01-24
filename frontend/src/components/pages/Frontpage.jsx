00f1409a67584b3015c5b1840494019ec5566d34
Aifert Yet <aifertyet0628@gmail.com>
2024-01-24 23:23:27 +0800

    big update to details and styling

--- frontend/src/components/pages/Frontpage.jsx
+++ frontend/src/components/pages/Frontpage.jsx
@@ -9,7 +9,7 @@ function Frontpage(props){
         <li className = "p-2"><a href = "./static_page/about.html">About</a></li>
         <li className = "p-2"><a href = "./static_page/socials.html">Socials</a></li>
         <li className = "p-2"><a href = "./static_page/education.html">Education</a></li>
-        <li className = "p-2"><a href = "./static_page/commit.html">Commitments</a></li>
+        <li className = "p-2"><a href = "./static_page/commit.html">Competitions</a></li>
         <li className = "p-2"><a href = "./static_page/work.html">Work Experience</a></li>
         <li className = "p-2"><a href = "./static_page/project.html">Projects</a></li>
         <li className = "p-2"><a href = "./static_page/skills.html">Skills</a></li>

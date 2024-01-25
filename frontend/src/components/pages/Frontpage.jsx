b28e74ca83f368b23b2020c9d19ce8d37ec135d3
Aifert Yet <aifertyet0628@gmail.com>
2024-01-25 18:55:36 +0800

    competitions done

--- frontend/src/components/pages/Frontpage.jsx
+++ frontend/src/components/pages/Frontpage.jsx
@@ -9,7 +9,7 @@ function Frontpage(props){
         <li className = "p-2"><a href = "#about">About</a></li>
         <li className = "p-2"><a href = "#skills">Skills</a></li>
         <li className = "p-2"><a href = "#education">Education</a></li>
-        <li className = "p-2"><a href = "#competitions">Competitions</a></li>
+        <li className = "p-2"><a href = "#education">Competitions</a></li>
         <li className = "p-2"><a href = "#work">Work Experience</a></li>
         <li className = "p-2"><a href = "#projects">Projects</a></li>
         <li className = "p-2"><a href = "#socials">Socials</a></li>
